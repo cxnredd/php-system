f4ce321e202bb146258c09e56f3c4101a8ecbbf3
Chrisxander Merilo <sayhi2q0d@gmail.com>
2025-02-23 23:20:13 +0800

    add

---
+++ sms4.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 20, 2025 at 02:30 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sms4`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_user`
+--
+
+CREATE TABLE `admin_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `usertype` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_user`
+--
+
+INSERT INTO `admin_user` (`id`, `username`, `password`, `usertype`) VALUES
+(2, 'admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_user`
+--
+
+CREATE TABLE `student_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
+  `usertype` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `student_user`
+--
+
+INSERT INTO `student_user` (`id`, `username`, `password`, `usertype`) VALUES
+(5, 'user', 'user', 'user'),
+(6, 'admin', 'admin', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student_user`
+--
+ALTER TABLE `student_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_user`
+--
+ALTER TABLE `admin_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `student_user`
+--
+ALTER TABLE `student_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ studentdb.php
@@ -0,0 +1,137 @@
+<?php 
+   session_start();
+
+   include("php/config.php");
+   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <title>Sms4</title>
+    <link rel="stylesheet" href="public/css/dstyle.css">
+    <link rel="stylesheet" href="public/css/style4.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/boxicons/2.0.5/css/boxicons.min.css">
+</head>
+<body>
+
+    <div class="sidenav" id="sidenav">
+        <h4 class="text-center" style="color: white; font-size: 20px;">Prefect Department</h4><br><br>
+        
+        <div class="avatar-container text-center">
+            <img src="public/assets/images/avatar.jfif" alt="Avatar" class="avatar">
+            <p class="username text-white">Juan Delacruz</p>
+            <p class="email text-white">juandelacruz@gmail.com</p>
+        </div>
+        
+        <div class="container mt-5">
+            <div class="dropdownSmsprofile">
+                <button class="dropdown-btn" onclick="toggleDropdown(this)">
+                <i class='bx bx-grid-alt'></i> Prefect of Discipline <i class="fa fa-caret-down" style="float: right;"></i>
+                </button>
+                <div class="dropdown-container">
+                    <a class="dropdown-a" href="studentdb.php" style="text-decoration: none;"><span>Dashboard</span></a>
+                    <a class="dropdown-a" href="academic.php" style="text-decoration: none;"><span>Violation Report</span></a>
+                    <a class="dropdown-a" href="discipline.php" style="text-decoration: none;"><span>Contact</span></a>
+                </div>
+            </div>
+        
+            <div class="dropdownSmsprofile">
+                <button class="dropdown-btn" onclick="toggleDropdown(this)">
+                    <i class='bx bx-user'></i> Student Information <i class="fa fa-caret-down" style="float: right;"></i>
+                </button>
+                <div class="dropdown-container">
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Student Personal Information</span></a>
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Student Personal Information</span></a>
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Student Personal Information</span></a>
+                </div>
+            </div>
+    
+            <div class="dropdownSmsprofile">
+                <button class="dropdown-btn" onclick="toggleDropdown(this)">
+                    <i class='bx bx-money'></i> Document <i class="fa fa-caret-down" style="float: right;"></i>
+                </button>
+                <div class="dropdown-container">
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Document Request</span></a>
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Document Request</span></a>
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Document Request</span></a>
+                </div>
+            </div>
+    
+            <div class="dropdownSmsprofile">
+                <button class="dropdown-btn" onclick="toggleDropdown(this)">
+                    <i class='bx bx-file'></i> Curriculum <i class="fa fa-caret-down" style="float: right;"></i>
+                </button>
+                <div class="dropdown-container">
+                    <a class="dropdown-a" href="#" style="text-decoration: none;"><span>Courses</span></a>
+                </div>
+            </div>
+    
+            
+    
+            <div class="dropdownSmsprofile">
+                <a class="dropdown-a" href="logout.php" style="text-decoration: none;"><i class='bx bx-log-out'></i> <span>Log Out</span></a>  
+            </div>
+        </div>
+    </div>
+    
+    
+<div class="main" id="mainContent">
+    <button class="btn" id="toggleButton">&nbsp; ☰ &nbsp;</button>
+    <hr>
+    <h2 class="text-left" style="font-size: 22px;">Dashboard</h2><br>
+    
+    <div class="main" id="mainContentOuter">
+        
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+
+const toggleButton = document.getElementById('toggleButton');
+const sidenav = document.getElementById('sidenav');
+const mainContent = document.getElementById('mainContent');
+const toggleButtonOuter = document.getElementById('toggleButtonOuter');
+const mainContentOuter = document.getElementById('mainContentOuter');
+
+// Attach event listener to the toggle button
+toggleButton.addEventListener('click', () => {
+  sidenav.classList.toggle('hidden');
+  mainContent.classList.toggle('shift');
+});
+
+// Attach event listener to the outer toggle button
+toggleButtonOuter.addEventListener('click', () => {
+  sidenav.classList.toggle('hidden');
+  mainContentOuter.classList.toggle('shift');
+});
+
+// Dropdown
+function toggleDropdown(button) {
+  button.classList.toggle("active");
+  var dropdownContent = button.nextElementSibling;
+  dropdownContent.style.display = (dropdownContent.style.display === "block") ? "none" : "block";
+}
+
+window.onclick = function(event){
+  if (!event.target.matches('.dropdown-btn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-container");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.style.display === "block"){
+        openDropdown.style.display = "none";
+      }
+    }
+  }
+};
+
+</script>
+</body>
+</html>
